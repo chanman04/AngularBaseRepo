7293e40ce81ffc932792e5fc10a231cbb269afcf
Michael <michaelchan985@gmail.com>
2019-02-08 20:08:56 -0600

    first push with AWS configured

---
+++ base-app/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import Amplify from 'aws-amplify';
+import aws_exports from '../aws-exports';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'base-app';
+
+  ngOnInit(){
+    Amplify.configure(aws_exports);
+    console.log('Amplify Initialised');
+    this.title = "Amplify Initialised";
+  }
+}
